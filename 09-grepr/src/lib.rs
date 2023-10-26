9addd1b9533a3ea077bc71c67c29d2e73cd2c673
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2023-10-25 21:41:16 -0600

    chap9: grepr: add color to output

--- 09-grepr/src/lib.rs
+++ 09-grepr/src/lib.rs
@@ -1,8 +1,14 @@
-use std::{error::Error, fs};
+use std::{
+    error::Error,
+    fs::{self, File},
+    io::{self, BufRead, BufReader},
+    mem,
+};
 
 use clap::Parser;
 use regex::{Regex, RegexBuilder};
 use walkdir::WalkDir;
+use owo_colors::{OwoColorize, colors::*};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -54,7 +60,37 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    let entries = find_files(&config.files, config.recursive);
+    let num_files = entries.len();
+
+    let print = |fname: &str, val: &str| {
+        if num_files > 1 {
+            print!("{}:{}", fname.fg::<Green>(), val);
+        } else {
+            print!("{}", val);
+        }
+    };
+
+    for entry in entries {
+        match entry {
+            Err(e) => eprintln!("{}", e),
+            Ok(filename) => match open(&filename) {
+                Err(e) => eprintln!("{}: {}", filename, e),
+                Ok(file) => match find_lines(file, &config.pattern, config.invert_match) {
+                    Err(e) => eprintln!("{}", e),
+                    Ok(matches) => {
+                        if config.count {
+                            print(&filename, &format!("{}\n", matches.len()));
+                        } else {
+                            for line in &matches {
+                                print(&filename, line);
+                            }
+                        }
+                    }
+                },
+            },
+        }
+    }
     Ok(())
 }
 
@@ -76,8 +112,10 @@ fn find_files(paths: &[String], recursive: bool) -> Vec<MyResult<String>> {
                                 results.push(Ok(entry.path().display().to_string()));
                             }
                         } else {
-                            results.push(Err(From::from(format!("{} is a directory", path))))
+                            results.push(Err(From::from(format!("{} is a directory", path))));
                         }
+                    } else if metadata.is_file() {
+                        results.push(Ok(path.to_string()));
                     }
                 }
 
@@ -91,11 +129,48 @@ fn find_files(paths: &[String], recursive: bool) -> Vec<MyResult<String>> {
     results
 }
 
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn find_lines<T: BufRead>(
+    mut file: T,
+    pattern: &Regex,
+    invert_match: bool,
+) -> MyResult<Vec<String>> {
+    let mut matches = vec![];
+    let mut line = String::new();
+
+    loop {
+        let bytes = file.read_line(&mut line)?;
+
+        if bytes == 0 {
+            break;
+        }
+
+        if pattern.is_match(&line) ^ invert_match {
+            matches.push(mem::take(&mut line));
+        }
+
+        line.clear();
+    }
+
+    Ok(matches)
+}
+
 // ---------------------------------------------------------
 #[cfg(test)]
 mod tests {
+    use crate::find_lines;
+
     use super::find_files;
     use rand::{distributions::Alphanumeric, Rng};
+    use regex::{Regex, RegexBuilder};
+    use std::io::Cursor;
+
     #[test]
     fn test_find_files() {
         // Verify that the function finds a file known to exist
@@ -137,5 +212,37 @@ mod tests {
         let files = find_files(&[bad], false);
         assert_eq!(files.len(), 1);
         assert!(files[0].is_err());
+    }
+
+    #[test]
+    fn test_find_lines() {
+        let text = b"Loren\nIpsum\r\nDOLOR";
+
+        // The pattern _or_ should match the one line, "Lorem"
+        let re1 = Regex::new("or").unwrap();
+        let matches = find_lines(Cursor::new(&text), &re1, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
+
+        // When inverted, the function should match the other two lines
+        let matches = find_lines(Cursor::new(&text), &re1, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+
+        // This regex will be case-insensitive
+        let re2 = RegexBuilder::new("or")
+            .case_insensitive(true)
+            .build()
+            .unwrap();
+
+        // The two lines "Lorem" and "DOLOR" should match
+        let matches = find_lines(Cursor::new(&text), &re2, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+
+        // When inverted, the one remaining line should match
+        let matches = find_lines(Cursor::new(&text), &re2, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
     }
 }
