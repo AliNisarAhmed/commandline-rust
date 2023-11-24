b4778cb877f1fb3050e643ce5cf5fa53c994832d
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2023-11-24 11:30:57 -0700

    chap13: calr

--- 13-calr/src/lib.rs
+++ 13-calr/src/lib.rs
@@ -1,7 +1,9 @@
 use std::{error::Error, str::FromStr};
 
+use ansi_term::Style;
 use chrono::{Datelike, Local, NaiveDate};
 use clap::Parser;
+use itertools::izip;
 
 const MONTH_NAMES: [&str; 12] = [
     "January",
@@ -17,6 +19,7 @@ const MONTH_NAMES: [&str; 12] = [
     "November",
     "December",
 ];
+const LINE_WIDTH: usize = 22;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -50,17 +53,108 @@ pub fn get_args() -> MyResult<Config> {
     let args = Args::parse();
 
     let today = Local::now();
-    let year = if args.show_current_year {
+    let year = if args.show_current_year || args.year.is_none() {
         today.year()
     } else {
         args.year.unwrap()
     };
 
+
     Ok(Config {
-        month: None,
-        year: year,
+        month: args.month,
+        year, 
         today: today.date_naive(),
     })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    match config.month {
+        Some(month) => {
+            let lines = format_month(config.year, month, true, config.today);
+            println!("{}", lines.join("\n"));
+        }
+        None => {
+            println!("{:>32}", config.year);
+            let months: Vec<_> = (1..=12)
+                .into_iter()
+                .map(|month| {
+                    format_month(config.year, month, false, config.today)
+                })
+                .collect();
+
+            for (i, chunk) in months.chunks(3).enumerate() {
+                if let [m1, m2, m3] = chunk {
+                    for lines in izip!(m1, m2, m3) {
+                        println!("{}{}{}", lines.0, lines.1, lines.2);
+                    }
+                    if i < 3 {
+                        println!();
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+// ----------------------------------------------------------------------------
+
+fn format_month(
+    year: i32,
+    month: u32,
+    print_year: bool,
+    today: NaiveDate
+    ) -> Vec<String> {
+    
+    let first = NaiveDate::from_ymd_opt(year, month, 1).unwrap();
+    let mut days: Vec<String> = (1..first.weekday().number_from_sunday())
+            .into_iter()
+            .map(|_| "  ".to_string())
+            .collect();
+
+    let is_today = |day: u32| {
+        year == today.year() && month == today.month() && day == today.day()
+    };
+
+    let last = last_day_in_month(year, month);
+
+    days.extend((first.day()..=last.day())
+                .into_iter()
+                .map(|num| {
+                    let fmt = format!("{:>2}", num);
+                    if is_today(num) {
+                        Style::new().reverse().paint(fmt).to_string()
+                    } else {
+                        fmt
+                    }
+                }));
+    
+    let month_name = MONTH_NAMES[month as usize - 1];
+    let mut lines = Vec::with_capacity(8);
+    lines.push(format!(
+            "{:^20}",
+            if print_year {
+                format!("{} {}", month_name, year)
+            } else {
+                month_name.to_string()
+            }
+            ));
+
+    lines.push("Su Mo Tu We Th Fr Sa  ".to_string());
+
+    for week in days.chunks(7) {
+        lines.push(format!(
+                "{:width$}  ",
+                week.join(" "),
+                width = LINE_WIDTH - 2
+                ));
+    }
+
+    while lines.len() < 8 {
+        lines.push(" ".repeat(LINE_WIDTH));
+    }
+
+    lines
 }
 
 fn parse_int<T: FromStr>(val: &str) -> Result<T, String> {
@@ -107,16 +201,35 @@ fn parse_month(month: &str) -> Result<u32, String> {
     }
 }
 
-// ----------------------------------------------------------------------------
-pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
-
-    Ok(())
+pub fn last_day_in_month(year: i32, month: u32) -> NaiveDate {
+    match month {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => {
+            NaiveDate::from_ymd_opt(year, month, 31).unwrap()
+        },
+        4 | 6 | 9 | 11 => {
+            NaiveDate::from_ymd_opt(year, month, 30).unwrap()
+        },
+        2 if is_leap_year(year) => {
+            NaiveDate::from_ymd_opt(year, month, 29).unwrap()
+        },
+        _ => {
+            NaiveDate::from_ymd_opt(year, month, 28).unwrap()
+        }
+    }
 }
+
+fn is_leap_year(year: i32) -> bool {
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+}
+
+
+// -----------------------------------------------------------------------------
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse_int, parse_month, parse_year};
+    use chrono::NaiveDate;
+
+    use crate::{parse_int, parse_month, parse_year, last_day_in_month};
 
     #[test]
     fn test_parse_int() {
@@ -187,5 +300,21 @@ mod tests {
         let res = parse_month("foo");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "Invalid month \"foo\"");
+    }
+
+    #[test]
+    fn test_last_day_in_month() {
+        assert_eq!(
+            last_day_in_month(2020, 1),
+            NaiveDate::from_ymd_opt(2020, 1, 31).unwrap()
+            );
+        assert_eq!(
+            last_day_in_month(2020, 2),
+            NaiveDate::from_ymd_opt(2020, 2, 29).unwrap()
+            );
+        assert_eq!(
+            last_day_in_month(2020, 4),
+            NaiveDate::from_ymd_opt(2020, 4, 30).unwrap()
+            );
     }
 }
